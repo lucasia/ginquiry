3fce6dbf6ff42b10bdbe1d86eb9b2c67919e6ead
Ian Lucas <ian.lucas@gmail.com>
2020-02-29 10:38:55 +0100

    Spring boot setup files

---
+++ src/test/java/com/lucasia/ginquiry/GinquiryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasia.ginquiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GinquiryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
