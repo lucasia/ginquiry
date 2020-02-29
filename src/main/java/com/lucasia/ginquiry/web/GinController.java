dc7036a96ed32c66588435b8d960dfa904869f26
Ian Lucas <ian.lucas@gmail.com>
2020-03-01 00:49:51 +0100

    slight clean-up of GinController

--- src/main/java/com/lucasia/ginquiry/web/GinController.java
+++ src/main/java/com/lucasia/ginquiry/web/GinController.java
@@ -1,21 +1,20 @@
 package com.lucasia.ginquiry.web;
 
 import com.lucasia.ginquiry.dao.BoozeRepository;
-import com.lucasia.ginquiry.dao.BrandRepository;
 import com.lucasia.ginquiry.domain.Booze;
-import com.lucasia.ginquiry.domain.Brand;
-import org.springframework.web.bind.annotation.*;
-
-import javax.transaction.Transactional;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(GinController.GIN_PATH)
-public class GinController extends AbstractController<Booze, Long> {
+@Log4j2
+public class GinController extends AbstractController<Booze, Long>{
 
     public static final String GIN_PATH = "/gins";
 
     public GinController(BoozeRepository boozeRepository) {
         super(boozeRepository);
     }
-
+    
 }
