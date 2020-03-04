8993b5e69cae84556659ff8a55d19ab9d4f31c2d
Ian Lucas <ian.lucas@gmail.com>
2020-03-04 15:04:16 +0100

    removed references to Feignclient

--- src/test/java/com/lucasia/ginquiry/GinquiryApplicationTests.java
+++ src/test/java/com/lucasia/ginquiry/GinquiryApplicationTests.java
@@ -1,35 +1,12 @@
 package com.lucasia.ginquiry;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
 
 @SpringBootTest
 public class GinquiryApplicationTests {
 
-	@Value("${"+ "ginquiry.server.port" +"}")
-	private int port;
-
-	@Autowired
-	Environment environment;
-
 	@Test
 	void contextLoads() {
 	}
-
-
-	@Test
-	void findProperty() {
-
-		System.out.println("port = " + port);
-
-
-		final String property = environment.getProperty("ginquiry.server.port");
-
-		System.out.println("property = " + property);
-
-	}
-
 }
